9547c35171051aca62b76bbe64bc953c733d61af
Jharilela <Jharilela@gmail.com>
2016-10-23 03:17:50 +0800

    change server lcoation

--- backend.js
+++ backend.js
@@ -3,7 +3,7 @@ http.createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type' : 'text/plain'});
 
 	response.end('hello world\n');
-}).listen(8080, '10.20.0.251')
+}).listen(8080, '0.0.0.0')
 console.log("server running at http:/127.0.0.1:8081/");
 
 // var express = require('express');
